1205c6ed42402ae61e429241bf76abc069161ee0
elmtree-Ahn <78303356+elmtree-Ahn@users.noreply.github.com>
2021-08-26 18:17:17 +0900

    4번까지 풀이

--- JS100/step1.js
+++ JS100/step1.js
@@ -1,1 +1,17 @@
+// 문제4
+// 2번
+
+// 문제3
+// let arr = [100, 200, 300];
+// console.log(typeof arr);
+
+// 문제2
+// let arr = [200, 100, 300];
+// arr.splice(2, 0, 10000);
+// console.log(arr);
+
 // 문제1
+// let nums = [100, 200, 300, 400, 500];
+// nums.pop();
+// nums.pop();
+// console.log(nums);

84a14263ed009d41a8bb970c1d85f3c52e71a124
elmtree-Ahn <78303356+elmtree-Ahn@users.noreply.github.com>
2021-08-28 22:13:53 +0900

    16번까지 풀이

--- JS100/step1.js
+++ JS100/step1.js
@@ -1,19 +1,76 @@
+// 문제 16
+// const name = prompt();
+// const nameReverse = name.split("").reverse().join("");
+// console.log(nameReverse);
+
+// 문제 15
+// const name = prompt();
+// console.log(`안녕하세요. 저는 ${name}입니다.`);
+
+// 문제 14
+// const data = prompt();
+
+// if (data % 3 === 0) {
+//   console.log("짝");
+// } else {
+//   console.log(data);
+// }
+
+// 문제13
+// const stars = [
+//   "수성",
+//   "금성",
+//   "지구",
+//   "화성",
+//   "목성",
+//   "토성",
+//   "천왕성",
+//   "해왕성"
+// ];
+// const input = prompt();
+
+// console.log(stars[input - 1]);
+
+// 문제12
+// class Wizard {
+//   constructor(health, mana, armor) {
+//     this.health = health;
+//     this.mana = mana;
+//     this.armor = armor;
+//   }
+//   attack() {
+//     console.log("파이어볼");
+//   }
+// }
+
+// const x = new Wizard(545, 210, 10);
+// console.log(x.health, x.mana, x.armor);
+// x.attack();
+
+// 문제11
+// let s = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   s += i;
+// }
+
+// console.log(s);
+
 // 문제10
-const n = prompt("숫자를 입력하세요.");
-let tree = "";
+// const n = prompt();
+// let tree = "";
 
-for (let i = 1; i <= n; i++) {
-  let star = "";
-  for (let j = 1; j <= n - i; j++) {
-    star += " ";
-  }
-  for (let k = 1; k <= 2 * i - 1; k++) {
-    star += "*";
-  }
-  tree += star + "\n";
-}
-
-console.log(tree);
+// for (let i = 1; i <= n; i++) {
+//   let star = "";
+//   for (let j = 1; i <= n - i; j++) {
+//     star += " ";
+//   }
+//   for (let k = 1; k <= i * 2 - 1; k++) {
+//     star += "*";
+//   }
+//   tree += star + "\n";
+// }
+// console.log(tree);
 
 // 문제9
 // let year = "2019";

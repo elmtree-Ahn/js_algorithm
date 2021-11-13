d284ee7f663bfdab0a7162cb6dd8b576234967f4
elmtree-Ahn <78303356+elmtree-Ahn@users.noreply.github.com>
2021-11-14 08:06:46 +0900

    34번까지 풀이

--- JS100/step1.js
+++ JS100/step1.js
@@ -1,6 +1,19 @@
-// 30
-const sentence = "pineapple is yummy";
-const word = "apple";
+// 34
+// const a = prompt();
+// const result = a.split(" ").sort().join(" ");
 
-console.log(`index = ${sentence.indexOf(word)}`);
-console.log(sentence.search(word));
+// if (a === result) {
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
+
+// 33 거 꾸 로
+// const data = prompt();
+// console.log(data.split("").reverse().join(""));
+
+// 32
+// const sentence = "안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다.";
+
+// const result = sentence.split(" ").length;
+// console.log(result);

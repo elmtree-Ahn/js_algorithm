cd3cd368a38c73cc26734b6a728d2c49627e389a
안유일 | Luffy <78303356+elmtree-Ahn@users.noreply.github.com>
2021-11-21 20:50:39 +0900

    37번 문제 풀이

--- JS100/step1.js
+++ JS100/step1.js
@@ -1,4 +1,18 @@
 // 37
+// const data = "원범 원범 혜원 혜원 혜원 혜원 유진 유진".split(" ");
+// let result = {};
+// let winner = "";
+
+// for (let index in data) {
+//   let name = data[index];
+//   result[name] = result[name] === undefined ? 1 : result[name] + 1;
+// }
+
+// winner = Object.keys(result).reduce((a, b) => {
+//   return result[a] > result[b] ? a : b;
+// });
+
+// console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
 
 // 36
 // const data = 2;

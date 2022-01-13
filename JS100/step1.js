1e6e3d4f06518e8d8799699237e0f67df3e648c9
안유일 | Luffy <78303356+elmtree-Ahn@users.noreply.github.com>
2022-01-13 21:28:03 +0900

    40번 문제 풀이

--- JS100/step1.js
+++ JS100/step1.js
@@ -1,6 +1,21 @@
+// 40
+let total = 0;
+let count = 0;
+const limit = prompt("제한 무게를 입력하세요.");
+const n = prompt("인원수를 입력하세요.");
+
+for (let i = 1; i <= n; i++) {
+  total += parseInt(prompt("무게를 입력해주세요."), 10);
+  if (total <= limit) {
+    count = i;
+  }
+}
+
+console.log(count);
+
 // 39
-const data = "hqllo my namq is hyqwon";
-console.log(data.split("q").join("e"));
+// const data = "hqllo my namq is hyqwon";
+// console.log(data.split("q").join("e"));
 
 // 38
 // const scores = "97 86 75 66 55 97 85 97 97 95".split(" ").sort();

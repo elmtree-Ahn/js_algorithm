28f68e4e652cc6f247f135ad2bf212a1efceb2a4
안유일 | Elmtree <78303356+elmtree-Ahn@users.noreply.github.com>
2022-01-17 21:17:25 +0900

    40번 문제 풀이

--- JS100/step1.js
+++ JS100/step1.js
@@ -1,13 +1,15 @@
 // 40
+const limit = Number(prompt("리미트를 입력하세요."));
+const person = Number(prompt("몇 명이 탑승하나요?"));
+
 let total = 0;
 let count = 0;
-const limit = prompt("제한 무게를 입력하세요.");
-const n = prompt("인원수를 입력하세요.");
 
-for (let i = 1; i <= n; i++) {
-  total += parseInt(prompt("무게를 입력해주세요."), 10);
+for (let i = 1; i <= 5; i++) {
+  let weight = Number(prompt("무게를 입력하세요"));
+  total = total + weight;
   if (total <= limit) {
-    count = i;
+    count++;
   }
 }
 
